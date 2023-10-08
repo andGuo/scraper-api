21fee16b48e4c4675cdad8fab062cbfacfac83a2
Andrew Guo <andrew.c.guo@gmail.com>
2023-10-07 22:55:03 -0400

    updated cors

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,24 @@ mod db;
 mod error;
 mod handler;
 mod model;
-mod route;
 mod response;
+mod route;
 
 use std::sync::Arc;
 
 use axum::http::{
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
-    HeaderValue, Method,
+    Method,
 };
 use db::DB;
 use dotenv::dotenv;
 use error::MyError;
 use route::create_router;
-use tower_http::{cors::CorsLayer, trace::TraceLayer};
 use std::net::SocketAddr;
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -30,20 +33,21 @@ async fn main() -> Result<(), MyError> {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var(
             "RUST_LOG",
-            "tower_http=debug",//",mongodb::command=debug",
+            "tower_http=debug", //",mongodb::command=debug",
         )
     }
 
     let db = DB::init().await?;
 
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin(Any)
         .allow_methods([Method::GET])
-        .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
     tracing_subscriber::fmt::init();
-    let app = create_router(Arc::new(AppState { db })).layer(cors).layer(TraceLayer::new_for_http());
+    let app = create_router(Arc::new(AppState { db }))
+        .layer(cors)
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
