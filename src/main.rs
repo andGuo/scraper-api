b9871c5b715732ee9a830f3b7b57468c35db7704
Andrew Guo <andrew.c.guo@gmail.com>
2023-10-09 01:57:06 -0400

    added .env variables for ip and port

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ mod response;
 mod route;
 mod schema;
 
-use std::sync::Arc;
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    sync::Arc,
+};
 
 use axum::http::{
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
@@ -18,9 +21,9 @@ use error::MyError;
 use route::create_router;
 use std::net::SocketAddr;
 use tower_http::{
+    catch_panic::CatchPanicLayer,
     cors::{Any, CorsLayer},
     trace::TraceLayer,
-    catch_panic::CatchPanicLayer,
 };
 
 #[derive(Clone)]
@@ -35,9 +38,25 @@ async fn main() -> Result<(), MyError> {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var(
             "RUST_LOG",
-            "tower_http=debug", //",mongodb::command=debug",
+            "info,tower_http=debug", //",mongodb::command=debug",
         )
     }
+
+    if std::env::var_os("RUST_SERVER_ADDRESS").is_none() {
+        std::env::set_var("RUST_SERVER_ADDRESS", "127.0.0.1")
+    }
+
+    if std::env::var_os("RUST_SERVER_PORT").is_none() {
+        std::env::set_var("RUST_SERVER_PORT", "3000")
+    }
+
+    let addr_str = std::env::var("RUST_SERVER_ADDRESS").expect("RUST_SERVER_ADDRESS must be set.");
+    let port_str = std::env::var("RUST_SERVER_PORT").expect("RUST_SERVER_PORT must be set.");
+    let addr: Ipv4Addr = addr_str.parse().expect("Invalid IPv4 address");
+    let port: u16 = port_str.parse().expect("Invalid port number");
+
+    // Create a SocketAddrV4 using the parsed IPv4 address and port
+    let socket_addr_v4 = SocketAddrV4::new(addr, port);
 
     let db = DB::init().await?;
 
@@ -52,7 +71,7 @@ async fn main() -> Result<(), MyError> {
         .layer(TraceLayer::new_for_http())
         .layer(CatchPanicLayer::new());
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(socket_addr_v4);
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
