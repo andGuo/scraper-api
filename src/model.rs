4feee9df42f520e842aba5463108775d1ebbd214
Andrew Guo <andrew.c.guo@gmail.com>
2023-10-16 17:27:23 -0400

    added Xkcd model and res

--- src/model.rs
+++ src/model.rs
@@ -3,6 +3,13 @@ use chrono::{DateTime, Utc};
 use mongodb::bson::{self, oid::ObjectId};
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ScoreDetails {
+    pub value: f64,
+    pub description: String,
+    pub details: Vec<ScoreDetails>,
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Fruit {
     #[serde(rename = "_id")]
@@ -21,9 +28,21 @@ pub struct Fruit {
     pub score: Option<ScoreDetails>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ScoreDetails {
-    pub value: f64,
-    pub description: String,
-    pub details: Vec<ScoreDetails>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Xkcd {
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
+    pub url: String,
+    #[serde(with = "chrono_datetime_as_bson_datetime")]
+    pub created_at: DateTime<Utc>,
+    #[serde(with = "chrono_datetime_as_bson_datetime")]
+    pub updated_at: DateTime<Utc>,
+    pub title: String,
+    pub keywords: Vec<String>,
+    pub text_content: String,
+    pub out_links: Vec<String>,
+    pub in_links: Vec<String>,
+    pub page_rank: f64,
+    pub score: Option<ScoreDetails>,
+    pub img_url: String,
 }
--- src/response.rs
+++ src/response.rs
@@ -1,4 +1,4 @@
-use crate::model::{Fruit, ScoreDetails};
+use crate::model::{Fruit, Xkcd, ScoreDetails};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -46,4 +46,47 @@ impl From<Fruit> for FruitResponse {
 pub struct FruitsResponse {
     pub status: &'static str,
     pub data: Vec<FruitResponse>,
+}
+
+#[derive(Serialize, Debug)]
+pub struct XkcdResponse {
+    pub id: String,
+    pub url: String,
+    pub created_at: String,
+    pub updated_at: String,
+    pub title: String,
+    pub keywords: Vec<String>,
+    pub text_content: String,
+    pub out_links: Vec<String>,
+    pub in_links: Vec<String>,
+    pub pr: f64,
+    pub score: Option<ScoreDetails>,
+    pub name: &'static str,
+    pub img_url: String,
+}
+
+impl From<Xkcd> for XkcdResponse {
+    fn from(comic: Xkcd) -> Self {
+        XkcdResponse {
+            id: comic.id.to_hex(),
+            url: comic.url,
+            created_at: comic.created_at.to_rfc3339(),
+            updated_at: comic.updated_at.to_rfc3339(),
+            title: comic.title,
+            keywords: comic.keywords,
+            text_content: comic.text_content,
+            out_links: comic.out_links,
+            in_links: comic.in_links,
+            pr: comic.page_rank,
+            score: comic.score,
+            name: "Andrew Guo",
+            img_url: comic.img_url,
+        }
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub struct XkcdsResponse {
+    pub status: &'static str,
+    pub data: Vec<XkcdResponse>,
 }
